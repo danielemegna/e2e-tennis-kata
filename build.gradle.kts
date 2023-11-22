9535421dc28df152e07e193b940e95823d80a969
Daniele Megna <daniele.megna@qmates.tech>
2023-11-22 08:09:43 +0100

    Setup empty ktor kotlin project with freemarker plugin

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+
+plugins {
+    kotlin("jvm") version "1.9.0"
+    id("io.ktor.plugin") version "2.3.6"
+}
+
+group = "it.danielemegna"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-jetty")
+    implementation("io.ktor:ktor-server-freemarker")
+    implementation("io.ktor:ktor-server-status-pages")
+    testImplementation(kotlin("test"))
+    testImplementation("org.assertj:assertj-core:3.24.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging.events(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+    outputs.upToDateWhen { false }
+}
+
+kotlin {
+    jvmToolchain(8)
+}
