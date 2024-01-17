71edec20ad128b625ff20fd44a2ed9e478e34094
Daniele Megna <daniele.megna@qmates.tech>
2024-01-17 08:15:31 +0100

    Register first player point and get updated scoreboard view

--- src/test/kotlin/acceptance/MatchAPITest.kt
+++ src/test/kotlin/acceptance/MatchAPITest.kt
@@ -9,10 +9,21 @@ class MatchAPITest {
 
     @Test
     fun `init new match on root path`(): Unit = runBlocking {
-        val doc = Jsoup.connect("http://localhost:8080").get()
+        val htmlPage = Jsoup.connect("http://localhost:8080").get()
 
-        val playersScoreboardRows = doc.select("#scoreboard tr")
+        val playersScoreboardRows = htmlPage.select("#scoreboard tr")
         assertThat(playersScoreboardRows[0].select("td.current-game").text()).isEqualTo("0")
+        assertThat(playersScoreboardRows[0].select("td.current-set").text()).isEqualTo("0")
+        assertThat(playersScoreboardRows[1].select("td.current-game").text()).isEqualTo("0")
+        assertThat(playersScoreboardRows[1].select("td.current-set").text()).isEqualTo("0")
+    }
+
+    @Test
+    fun `register first player point`(): Unit = runBlocking {
+        val htmlPage = Jsoup.connect("http://localhost:8080/player/1/point").post()
+
+        val playersScoreboardRows = htmlPage.select("#scoreboard tr")
+        assertThat(playersScoreboardRows[0].select("td.current-game").text()).isEqualTo("15")
         assertThat(playersScoreboardRows[0].select("td.current-set").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[1].select("td.current-game").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[1].select("td.current-set").text()).isEqualTo("0")
