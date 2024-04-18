e08fea343297335ea39878e7b32551feb571f2fb
Daniele Megna <daniele.megna@qmates.tech>
2024-04-18 07:19:49 +0200

    Add disabled more complete scenario acceptance test

--- src/test/kotlin/acceptance/MatchAPITest.kt
+++ src/test/kotlin/acceptance/MatchAPITest.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.jsoup.Connection.Method
 import org.jsoup.Jsoup
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class MatchAPITest {
@@ -34,6 +35,25 @@ class MatchAPITest {
         assertThat(playersScoreboardRows[0].select("td.current-set").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[1].select("td.current-game").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[1].select("td.current-set").text()).isEqualTo("0")
+    }
+
+    @Disabled("WIP more complete scenario")
+    @Test
+    fun `register some points in first game`(): Unit = runBlocking {
+        initNewMatch()
+
+        Jsoup.connect("http://localhost:8080/player/1/point").method(Method.POST).execute()
+        Jsoup.connect("http://localhost:8080/player/1/point").method(Method.POST).execute()
+        Jsoup.connect("http://localhost:8080/player/2/point").method(Method.POST).execute()
+        Jsoup.connect("http://localhost:8080/player/1/point").method(Method.POST).execute().let { response ->
+            assertThat(response.statusCode()).isEqualTo(200)
+            val htmlPage = response.parse()
+            val playersScoreboardRows = htmlPage.select("#scoreboard tr")
+            assertThat(playersScoreboardRows[0].select("td.current-game").text()).isEqualTo("40")
+            assertThat(playersScoreboardRows[0].select("td.current-set").text()).isEqualTo("0")
+            assertThat(playersScoreboardRows[1].select("td.current-game").text()).isEqualTo("15")
+            assertThat(playersScoreboardRows[1].select("td.current-set").text()).isEqualTo("0")
+        }
     }
 
     private fun initNewMatch() {
