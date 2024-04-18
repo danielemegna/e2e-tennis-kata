f377c7646ee201ed41f4f51e394994078d40ae07
Daniele Megna <daniele.megna@qmates.tech>
2024-04-18 07:11:18 +0200

    Improve MatchAPITest
    - use Jsoup execute to obtain and check response status code
    - init a new match before trying to register first player points

--- src/test/kotlin/acceptance/MatchAPITest.kt
+++ src/test/kotlin/acceptance/MatchAPITest.kt
@@ -2,6 +2,7 @@ package acceptance
 
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.jsoup.Connection.Method
 import org.jsoup.Jsoup
 import org.junit.jupiter.api.Test
 
@@ -9,8 +10,10 @@ class MatchAPITest {
 
     @Test
     fun `init new match on root path`(): Unit = runBlocking {
-        val htmlPage = Jsoup.connect("http://localhost:8080").get()
+        val response = Jsoup.connect("http://localhost:8080").method(Method.GET).execute()
 
+        assertThat(response.statusCode()).isEqualTo(200)
+        val htmlPage = response.parse()
         val playersScoreboardRows = htmlPage.select("#scoreboard tr")
         assertThat(playersScoreboardRows[0].select("td.current-game").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[0].select("td.current-set").text()).isEqualTo("0")
@@ -20,12 +23,20 @@ class MatchAPITest {
 
     @Test
     fun `register first player point`(): Unit = runBlocking {
-        val htmlPage = Jsoup.connect("http://localhost:8080/player/1/point").post()
+        initNewMatch()
 
+        val response = Jsoup.connect("http://localhost:8080/player/1/point").method(Method.POST).execute()
+
+        assertThat(response.statusCode()).isEqualTo(200)
+        val htmlPage = response.parse()
         val playersScoreboardRows = htmlPage.select("#scoreboard tr")
         assertThat(playersScoreboardRows[0].select("td.current-game").text()).isEqualTo("15")
         assertThat(playersScoreboardRows[0].select("td.current-set").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[1].select("td.current-game").text()).isEqualTo("0")
         assertThat(playersScoreboardRows[1].select("td.current-set").text()).isEqualTo("0")
+    }
+
+    private fun initNewMatch() {
+        Jsoup.connect("http://localhost:8080").method(Method.GET).execute()
     }
 }
