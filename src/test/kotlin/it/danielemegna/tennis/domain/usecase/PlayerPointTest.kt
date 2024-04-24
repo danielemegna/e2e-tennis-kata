ab42aec00bb1fb22473a0b7f42ccbe9734bb8bed
Daniele Megna <megna.dany@gmail.com>
2024-04-24 08:12:51 +0200

    Create test gropus in PlayerPoint usecase unit tests

--- src/test/kotlin/it/danielemegna/tennis/domain/usecase/PlayerPointTest.kt
+++ src/test/kotlin/it/danielemegna/tennis/domain/usecase/PlayerPointTest.kt
@@ -5,126 +5,142 @@ import it.danielemegna.tennis.domain.MatchState.Game.GameScore
 import it.danielemegna.tennis.domain.MatchState.Game.GameScore.*
 import it.danielemegna.tennis.domain.MatchState.Serving
 import it.danielemegna.tennis.domain.repository.StubMatchRepository
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class PlayerPointTest {
 
-    @Test
-    fun `first player point on new game`() {
-        val newMatchState = MatchState("p1", "p2")
+    @Nested
+    inner class GameNormalPoint {
 
-        val updatedMatchState = updatedMatchStateFor(newMatchState, PlayerPoint.Player.FIRST)
+        @Test
+        fun `first player point on new game`() {
+            val newMatchState = MatchState("p1", "p2")
 
-        assertEquals(FIFTEEN, updatedMatchState.currentGame.firstPlayerScore)
-        assertEquals(ZERO, updatedMatchState.currentGame.secondPlayerScore)
+            val updatedMatchState = updatedMatchStateFor(newMatchState, PlayerPoint.Player.FIRST)
+
+            assertEquals(FIFTEEN, updatedMatchState.currentGame.firstPlayerScore)
+            assertEquals(ZERO, updatedMatchState.currentGame.secondPlayerScore)
+        }
+
+        @Test
+        fun `second player point on new game`() {
+            val newMatchState = MatchState("p1", "p2")
+
+            val updatedMatchState = updatedMatchStateFor(newMatchState, PlayerPoint.Player.SECOND)
+
+            assertEquals(FIFTEEN, updatedMatchState.currentGame.secondPlayerScore)
+            assertEquals(ZERO, updatedMatchState.currentGame.firstPlayerScore)
+        }
+
+        @Test
+        fun `first player point on progress game`() {
+            val matchState = MatchState("p1", "p2").copy(
+                currentGame = MatchState.Game(THIRTY, FIFTEEN)
+            )
+
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
+
+            assertEquals(FORTY, updatedMatchState.currentGame.firstPlayerScore)
+            assertEquals(FIFTEEN, updatedMatchState.currentGame.secondPlayerScore)
+        }
+
+        @Test
+        fun `second player point on progress game`() {
+            val matchState = MatchState("p1", "p2").copy(
+                currentGame = MatchState.Game(THIRTY, FIFTEEN)
+            )
+
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
+
+            assertEquals(THIRTY, updatedMatchState.currentGame.secondPlayerScore)
+            assertEquals(THIRTY, updatedMatchState.currentGame.firstPlayerScore)
+        }
+
     }
 
-    @Test
-    fun `second player point on new game`() {
-        val newMatchState = MatchState("p1", "p2")
+    @Nested
+    inner class GameWinning {
 
-        val updatedMatchState = updatedMatchStateFor(newMatchState, PlayerPoint.Player.SECOND)
+        @Test
+        fun `game won by first player`() {
+            val matchState = MatchState("p1", "p2").copy(
+                currentGame = MatchState.Game(FORTY, FIFTEEN)
+            )
 
-        assertEquals(FIFTEEN, updatedMatchState.currentGame.secondPlayerScore)
-        assertEquals(ZERO, updatedMatchState.currentGame.firstPlayerScore)
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
+
+            assertEquals(1, updatedMatchState.currentSet.firstPlayerScore)
+            assertEquals(0, updatedMatchState.currentSet.secondPlayerScore)
+            assertEquals(MatchState.Game(ZERO, ZERO), updatedMatchState.currentGame)
+        }
+
+        @Test
+        fun `game won by second player`() {
+            val matchState = MatchState("p1", "p2").copy(
+                currentGame = MatchState.Game(THIRTY, FORTY)
+            )
+
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
+
+            assertEquals(1, updatedMatchState.currentSet.secondPlayerScore)
+            assertEquals(0, updatedMatchState.currentSet.firstPlayerScore)
+            assertEquals(MatchState.Game(ZERO, ZERO), updatedMatchState.currentGame)
+        }
+
     }
 
-    @Test
-    fun `first player point on progress game`() {
-        val matchState = MatchState("p1", "p2").copy(
-            currentGame = MatchState.Game(THIRTY, FIFTEEN)
-        )
+    @Nested
+    inner class ServingPlayerUpdate {
 
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
+        @Test
+        fun `first player still serving in ongoing first game`() {
+            val matchState = MatchState("p1", "p2").copy(
+                serving = Serving.FIRST_PLAYER
+            )
 
-        assertEquals(FORTY, updatedMatchState.currentGame.firstPlayerScore)
-        assertEquals(FIFTEEN, updatedMatchState.currentGame.secondPlayerScore)
-    }
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
 
-    @Test
-    fun `second player point on progress game`() {
-        val matchState = MatchState("p1", "p2").copy(
-            currentGame = MatchState.Game(THIRTY, FIFTEEN)
-        )
+            assertEquals(Serving.FIRST_PLAYER, updatedMatchState.serving)
+        }
 
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
+        @Test
+        fun `second player serve when a game served by first player is completed`() {
+            val matchState = MatchState("p1", "p2").copy(
+                serving = Serving.FIRST_PLAYER,
+                currentGame = MatchState.Game(FORTY, ZERO)
+            )
 
-        assertEquals(THIRTY, updatedMatchState.currentGame.secondPlayerScore)
-        assertEquals(THIRTY, updatedMatchState.currentGame.firstPlayerScore)
-    }
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
 
-    @Test
-    fun `game won by first player`() {
-        val matchState = MatchState("p1", "p2").copy(
-            currentGame = MatchState.Game(FORTY, FIFTEEN)
-        )
+            assertEquals(Serving.SECOND_PLAYER, updatedMatchState.serving)
+        }
 
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
+        @Test
+        fun `first player serve when a game served by second player is completed`() {
+            val matchState = MatchState("p1", "p2").copy(
+                serving = Serving.SECOND_PLAYER,
+                currentGame = MatchState.Game(FIFTEEN, FORTY)
+            )
 
-        assertEquals(1, updatedMatchState.currentSet.firstPlayerScore)
-        assertEquals(0, updatedMatchState.currentSet.secondPlayerScore)
-        assertEquals(MatchState.Game(ZERO, ZERO), updatedMatchState.currentGame)
-    }
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
 
-    @Test
-    fun `game won by second player`() {
-        val matchState = MatchState("p1", "p2").copy(
-            currentGame = MatchState.Game(THIRTY, FORTY)
-        )
+            assertEquals(Serving.FIRST_PLAYER, updatedMatchState.serving)
+        }
 
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
+        @Test
+        fun `first player serve also when a game served by second player is won by first player`() {
+            val matchState = MatchState("p1", "p2").copy(
+                serving = Serving.SECOND_PLAYER,
+                currentGame = MatchState.Game(FORTY, ZERO)
+            )
 
-        assertEquals(1, updatedMatchState.currentSet.secondPlayerScore)
-        assertEquals(0, updatedMatchState.currentSet.firstPlayerScore)
-        assertEquals(MatchState.Game(ZERO, ZERO), updatedMatchState.currentGame)
-    }
+            val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
 
-    @Test
-    fun `first player still serving in ongoing first game`() {
-        val matchState = MatchState("p1", "p2").copy(
-            serving = Serving.FIRST_PLAYER
-        )
+            assertEquals(Serving.FIRST_PLAYER, updatedMatchState.serving)
+        }
 
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
-
-        assertEquals(Serving.FIRST_PLAYER, updatedMatchState.serving)
-    }
-
-    @Test
-    fun `second player serve when a game served by first player is completed`() {
-        val matchState = MatchState("p1", "p2").copy(
-            serving = Serving.FIRST_PLAYER,
-            currentGame = MatchState.Game(FORTY, ZERO)
-        )
-
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
-
-        assertEquals(Serving.SECOND_PLAYER, updatedMatchState.serving)
-    }
-
-    @Test
-    fun `first player serve when a game served by second player is completed`() {
-        val matchState = MatchState("p1", "p2").copy(
-            serving = Serving.SECOND_PLAYER,
-            currentGame = MatchState.Game(FIFTEEN, FORTY)
-        )
-
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.SECOND)
-
-        assertEquals(Serving.FIRST_PLAYER, updatedMatchState.serving)
-    }
-
-    @Test
-    fun `first player serve also when a game served by second player is won by first player`() {
-        val matchState = MatchState("p1", "p2").copy(
-            serving = Serving.SECOND_PLAYER,
-            currentGame = MatchState.Game(FORTY, ZERO)
-        )
-
-        val updatedMatchState = updatedMatchStateFor(matchState, PlayerPoint.Player.FIRST)
-
-        assertEquals(Serving.FIRST_PLAYER, updatedMatchState.serving)
     }
 
     private fun updatedMatchStateFor(matchState: MatchState, pointAuthor: PlayerPoint.Player): MatchState {
