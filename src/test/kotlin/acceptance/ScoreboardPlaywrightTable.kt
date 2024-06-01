083cb39aaaacc6ca1a00048759e3e0eeedbaade2
Daniele Megna <megna.dany@gmail.com>
2024-06-01 14:14:41 +0200

    Improve also ScoreboardPlaywrightTable consistently

--- src/test/kotlin/acceptance/ScoreboardPlaywrightTable.kt
+++ src/test/kotlin/acceptance/ScoreboardPlaywrightTable.kt
@@ -5,31 +5,25 @@ import com.microsoft.playwright.Page
 import com.microsoft.playwright.assertions.LocatorAssertions
 import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
 import com.microsoft.playwright.options.AriaRole.*
+import org.junit.jupiter.api.Assertions.assertTrue
 import java.util.regex.Pattern
-import kotlin.test.assertEquals
 
 class ScoreboardPlaywrightTable private constructor(scoreboardTable: Locator) {
 
-    val firstPlayer: ScoreboardPlaywrightPlayerRow
-    val secondPlayer: ScoreboardPlaywrightPlayerRow
+    private val tableRows: Locator = scoreboardTable.getByRole(ROW)
+
+    val firstPlayer get() = ScoreboardPlaywrightPlayerRow(tableRows.nth(0))
+    val secondPlayer get() = ScoreboardPlaywrightPlayerRow(tableRows.nth(1))
 
     init {
-        val tableRows = scoreboardTable.getByRole(ROW)
-        assertEquals(2, tableRows.count(), "Unexpected table rows count: ${tableRows.count()}")
-        firstPlayer = ScoreboardPlaywrightPlayerRow(tableRows.nth(0))
-        secondPlayer = ScoreboardPlaywrightPlayerRow(tableRows.nth(1))
+        assertThat(tableRows).hasCount(2)
     }
 
     fun firstPlayerPoint() = this.firstPlayer.playerName.click()
     fun secondPlayerPoint() = this.secondPlayer.playerName.click()
 
     companion object {
-        fun from(page: Page): ScoreboardPlaywrightTable {
-            val scoreboardTable = page.getByRole(TABLE)
-            assertEquals(1, scoreboardTable.count(), "Unexpected table count: ${scoreboardTable.count()}")
-            assertThat(scoreboardTable).isVisible()
-            return ScoreboardPlaywrightTable(scoreboardTable.first())
-        }
+        fun from(page: Page) = ScoreboardPlaywrightTable(page.getByRole(TABLE))
     }
 
 }
