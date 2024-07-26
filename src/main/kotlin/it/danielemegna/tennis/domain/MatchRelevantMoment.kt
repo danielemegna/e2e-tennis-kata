aad5b0c52d7937568cefffca3846147466ab5aad
Daniele Megna <megna.dany@gmail.com>
2024-07-26 10:03:20 +0200

    Developing MatchRelevantMoment .. not convinced
    we are going to duplicate lot of logic already present in
    MatchStateUpdater ... maybe we should extract and reuse something

--- src/main/kotlin/it/danielemegna/tennis/domain/MatchRelevantMoment.kt
+++ src/main/kotlin/it/danielemegna/tennis/domain/MatchRelevantMoment.kt
@@ -1,27 +1,34 @@
 package it.danielemegna.tennis.domain
 
+import it.danielemegna.tennis.domain.MatchState.Game.GameScore
 import it.danielemegna.tennis.domain.MatchState.Game.GameScore.FORTY
-import it.danielemegna.tennis.domain.MatchState.Game.GameScore.THIRTY
+import kotlin.math.abs
 
 data class MatchRelevantMoment(
     val kind: Kind,
     val counter: Int? = null
 ) {
-
     companion object {
         fun from(matchState: MatchState): MatchRelevantMoment? {
-            if (closeToSetPoint(matchState)) {
-                return MatchRelevantMoment(Kind.SET_POINT, 2)
+            val isAnyoneForty =
+                matchState.currentGame.firstPlayerScore == FORTY || matchState.currentGame.secondPlayerScore == FORTY
+            val currentGameDifference =
+                abs(matchState.currentGame.firstPlayerScore - matchState.currentGame.secondPlayerScore)
+            val isAnyoneOverFour =
+                matchState.currentSet.firstPlayerScore > 4 || matchState.currentSet.secondPlayerScore > 4
+            if (isAnyoneForty && isAnyoneOverFour && currentGameDifference > 1) {
+                return MatchRelevantMoment(Kind.SET_POINT, currentGameDifference)
             }
 
             return null
         }
 
-        private fun closeToSetPoint(matchState: MatchState): Boolean {
-            return matchState.currentGame.firstPlayerScore == FORTY &&
-                matchState.currentGame.secondPlayerScore < THIRTY
-        }
     }
 
     enum class Kind { SET_POINT }
 }
+
+private operator fun GameScore.minus(other: GameScore): Int {
+    return this.ordinal - other.ordinal
+}
+
--- src/test/kotlin/it/danielemegna/tennis/domain/MatchRelevantMomentTest.kt
+++ src/test/kotlin/it/danielemegna/tennis/domain/MatchRelevantMomentTest.kt
@@ -16,7 +16,17 @@ class MatchRelevantMomentTest {
     }
 
     @Test
-    fun `one set point is not a relevant moment - doubt`() {
+    fun `a normal game point`() {
+        val matchState = MatchState("p1", "p2").copy(
+            currentGame = Game(FORTY, FIFTEEN),
+            currentSet = Set(firstPlayerScore = 2, secondPlayerScore = 2)
+        )
+        val actual = MatchRelevantMoment.from(matchState)
+        assertThat(actual).isNull()
+    }
+
+    @Test
+    fun `one set point is not a relevant moment`() {
         val matchState = MatchState("p1", "p2").copy(
             currentGame = Game(FORTY, THIRTY),
             currentSet = Set(firstPlayerScore = 5, secondPlayerScore = 2)
@@ -35,6 +45,19 @@ class MatchRelevantMomentTest {
         val actual = MatchRelevantMoment.from(matchState)
 
         val expected = MatchRelevantMoment(kind = MatchRelevantMoment.Kind.SET_POINT, counter = 2)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `three set point`() {
+        val matchState = MatchState("p1", "p2").copy(
+            currentGame = Game(ZERO, FORTY),
+            currentSet = Set(firstPlayerScore = 4, secondPlayerScore = 5)
+        )
+
+        val actual = MatchRelevantMoment.from(matchState)
+
+        val expected = MatchRelevantMoment(kind = MatchRelevantMoment.Kind.SET_POINT, counter = 3)
         assertThat(actual).isEqualTo(expected)
     }
 
