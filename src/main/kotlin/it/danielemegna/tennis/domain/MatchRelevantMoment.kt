09dfe41309e1849937a6439e39209dafc1144753
Daniele Megna <daniele.megna@qmates.tech>
2024-08-07 08:06:59 +0200

    Integrate MatchStateScanner in MatchRelevantMoment
    obviously i don't like this static vs non-static problem
    with this `from` MatchRelevantMoment factory method

--- src/main/kotlin/it/danielemegna/tennis/domain/MatchRelevantMoment.kt
+++ src/main/kotlin/it/danielemegna/tennis/domain/MatchRelevantMoment.kt
@@ -2,6 +2,7 @@ package it.danielemegna.tennis.domain
 
 import it.danielemegna.tennis.domain.MatchState.Game.GameScore
 import it.danielemegna.tennis.domain.MatchState.Game.GameScore.FORTY
+import it.danielemegna.tennis.domain.usecase.PlayerPoint
 import kotlin.math.abs
 
 data class MatchRelevantMoment(
@@ -10,15 +11,19 @@ data class MatchRelevantMoment(
 ) {
     companion object {
         fun from(matchState: MatchState): MatchRelevantMoment? {
-            val isAnyoneForty =
-                matchState.currentGame.firstPlayerScore == FORTY || matchState.currentGame.secondPlayerScore == FORTY
-            val currentGameDifference =
-                abs(matchState.currentGame.firstPlayerScore - matchState.currentGame.secondPlayerScore)
-            val isAnyoneOverFour =
-                matchState.currentSet.firstPlayerScore > 4 || matchState.currentSet.secondPlayerScore > 4
-            if (isAnyoneForty && isAnyoneOverFour && currentGameDifference > 1) {
-                return MatchRelevantMoment(Kind.SET_POINT, currentGameDifference)
+            val matchStateScanner = MatchStateScanner()
+            val currentGameDifference = abs(
+                matchState.currentGame.firstPlayerScore - matchState.currentGame.secondPlayerScore
+            )
+
+            if (matchStateScanner.wouldWinTieBreak(matchState)) {
+                val tieBreak = matchState.currentSet.tieBreak!!
+                val tieBreakDifference = abs(tieBreak.firstPlayerScore - tieBreak.secondPlayerScore)
+                return MatchRelevantMoment(Kind.SET_POINT, tieBreakDifference)
             }
+
+            if (matchStateScanner.wouldBeSetPoint(matchState) && currentGameDifference > 1)
+                return MatchRelevantMoment(Kind.SET_POINT, currentGameDifference)
 
             return null
         }
@@ -26,6 +31,16 @@ data class MatchRelevantMoment(
     }
 
     enum class Kind { SET_POINT }
+}
+
+private fun MatchStateScanner.wouldWinTieBreak(matchState: MatchState): Boolean {
+    return wouldWinTieBreak(matchState, PlayerPoint.Player.FIRST) ||
+        wouldWinTieBreak(matchState, PlayerPoint.Player.SECOND)
+}
+
+private fun MatchStateScanner.wouldBeSetPoint(matchState: MatchState): Boolean {
+    return wouldBeSetPoint(matchState, PlayerPoint.Player.FIRST) ||
+        wouldBeSetPoint(matchState, PlayerPoint.Player.SECOND)
 }
 
 private operator fun GameScore.minus(other: GameScore): Int {
--- src/test/kotlin/it/danielemegna/tennis/domain/MatchRelevantMomentTest.kt
+++ src/test/kotlin/it/danielemegna/tennis/domain/MatchRelevantMomentTest.kt
@@ -1,6 +1,6 @@
 package it.danielemegna.tennis.domain
 
-import it.danielemegna.tennis.domain.MatchState.Game
+import it.danielemegna.tennis.domain.MatchState.*
 import it.danielemegna.tennis.domain.MatchState.Game.GameScore.*
 import it.danielemegna.tennis.domain.MatchState.Set
 import org.assertj.core.api.Assertions.assertThat
@@ -58,6 +58,38 @@ class MatchRelevantMomentTest {
         val actual = MatchRelevantMoment.from(matchState)
 
         val expected = MatchRelevantMoment(kind = MatchRelevantMoment.Kind.SET_POINT, counter = 3)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `one set point at tiebreak is a relevant moment`() {
+        val matchState = MatchState("p1", "p2").copy(
+            currentSet = Set(
+                firstPlayerScore = 6,
+                secondPlayerScore = 6,
+                tieBreak = TieBreak(Serving.FIRST_PLAYER, 8, 9)
+            ),
+        )
+
+        val actual = MatchRelevantMoment.from(matchState)
+
+        val expected = MatchRelevantMoment(kind = MatchRelevantMoment.Kind.SET_POINT, counter = 1)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `four set point at tiebreak`() {
+        val matchState = MatchState("p1", "p2").copy(
+            currentSet = Set(
+                firstPlayerScore = 6,
+                secondPlayerScore = 6,
+                tieBreak = TieBreak(Serving.FIRST_PLAYER, 6, 2)
+            ),
+        )
+
+        val actual = MatchRelevantMoment.from(matchState)
+
+        val expected = MatchRelevantMoment(kind = MatchRelevantMoment.Kind.SET_POINT, counter = 4)
         assertThat(actual).isEqualTo(expected)
     }
 
